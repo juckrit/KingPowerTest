2e793daac8702a3b0d53a7dbd986929f1ffbb3c3
sam <juckrit072@gmail.com>
2020-12-12 16:17:57 +0700

    add glide listener

--- app/src/main/java/com/example/kingpowertest/presentation/main/adapter/PhotoAdapter.kt
+++ app/src/main/java/com/example/kingpowertest/presentation/main/adapter/PhotoAdapter.kt
@@ -1,9 +1,15 @@
 package com.example.kingpowertest.presentation.main.adapter
 
+import android.graphics.drawable.Drawable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.kingpowertest.R
 import com.example.kingpowertest.data.model.PhotoModel
 import com.example.kingpowertest.databinding.ItemListPhotoBinding
@@ -41,7 +47,38 @@ class PhotoAdapter :
                 .load(model.thumbnailUrl)
                 .error(R.color.colorBlack)
                 .placeholder(R.color.colorPrimary)
+                .listener(object:RequestListener<Drawable>{
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        Log.d("TEST",e.toString())
+                        Log.d("TEST",model.toString())
+                        Log.d("TEST",target.toString())
+                        Log.d("TEST",isFirstResource.toString())
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        Log.d("TEST",resource.toString())
+                        Log.d("TEST",model.toString())
+                        Log.d("TEST",target.toString())
+                        Log.d("TEST",dataSource.toString())
+                        Log.d("TEST",isFirstResource.toString())
+                        return false
+
+                    }
+                })
                 .into(binding.iv)
+
             binding.tv.text = model.title
         }
     }
