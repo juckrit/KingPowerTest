100bbf15f9e12e2bd01d37f8a3cc5dacf556b137
sam <juckrit072@gmail.com>
2020-12-12 15:03:31 +0700

    add anim and update nav

--- app/src/main/java/com/example/kingpowertest/data/model/model.kt
+++ app/src/main/java/com/example/kingpowertest/data/model/model.kt
@@ -1,12 +1,16 @@
 package com.example.kingpowertest.data.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class PhotoModel(
     @SerializedName("albumId") @Expose val albumId: Int,
     @SerializedName("id") @Expose val photoId: Int,
     @SerializedName("title") @Expose val title: String,
     @SerializedName("url") @Expose val url: String,
     @SerializedName("thumbnailUrl") @Expose val thumbnailUrl: String
-)
+): Parcelable
+
