323f72bb23547683dfae56985243099e666ec682
sam <juckrit072@gmail.com>
2020-12-13 19:49:22 +0700

    test fetchData_returnValueAsNull_Then_ShowMessageAs_FAILURE

--- app/src/test/java/com/example/kingpowertest/presentation/main/viewmodel/MainFragmentViewModelTest.kt
+++ app/src/test/java/com/example/kingpowertest/presentation/main/viewmodel/MainFragmentViewModelTest.kt
@@ -2,28 +2,34 @@ package com.example.kingpowertest.presentation.main.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.kingpowertest.FAILURE
 import com.example.kingpowertest.data.model.ResultWrapper
-import com.example.kingpowertest.data.repository.FakePhotoRepository
+import com.example.kingpowertest.domain.repository.PhotoRepository
 import com.example.kingpowertest.domain.usecase.GetPhotoUseCase
 import com.example.kingpowertest.getOrAwaitValue
 import com.example.kingpowertest.presentation.main.mapper.PhotoNetworkModelMapper
 import com.example.kingpowertest.presentation.main.model.PhotoPresentationModel
 import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+
 
 @RunWith(AndroidJUnit4::class)
 class MainFragmentViewModelTest {
     private lateinit var viewModel: MainFragmentViewModel
+    private val fakeRepository = Mockito.mock(PhotoRepository::class.java)
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Before
     fun setUp() {
-        val fakeRepository = FakePhotoRepository()
         val getPhotoUseCase = GetPhotoUseCase(fakeRepository)
         val mapper = PhotoNetworkModelMapper()
         viewModel = MainFragmentViewModel(getPhotoUseCase, mapper)
@@ -43,6 +49,23 @@ class MainFragmentViewModelTest {
             }
             is ResultWrapper.Error -> {
 
+            }
+        }
+    }
+
+    @Test
+    fun fetchData_returnValueAsNull_Then_ShowMessageAs_FAILURE() {
+        runBlocking {
+            `when`(fakeRepository.getPhotos(1)).thenReturn(null)
+        }
+        viewModel.getPhotoByAlbumId(1)
+        val currentList = viewModel.getPhotoListLiveData().getOrAwaitValue()
+        when (currentList) {
+            is ResultWrapper.Success -> {
+
+            }
+            is ResultWrapper.Error -> {
+                assertThat(currentList.message).isEqualTo(FAILURE)
             }
         }
     }
