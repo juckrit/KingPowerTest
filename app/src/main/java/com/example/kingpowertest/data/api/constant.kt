c5dce062ec041138cc2a9d5563310fa3aecb17b1
sam <juckrit072@gmail.com>
2020-12-14 01:24:20 +0700

    remove unused file

--- app/src/main/java/com/example/kingpowertest/data/api/constant.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kingpowertest.data.api
-
-import com.example.kingpowertest.BuildConfig
-
-//https://jsonplaceholder.typicode.com/albums/1/photos
-//const val BASE_ALBUM_URL = BuildConfig.BASE_URL + "albums/"
