96e46a906a81c2b4f17fe28ebaa317510cca14fd
sam <juckrit072@gmail.com>
2020-12-13 18:53:53 +0700

    update di

--- app/src/main/java/com/example/kingpowertest/presentation/di/AppModule.kt
+++ app/src/main/java/com/example/kingpowertest/presentation/di/AppModule.kt
@@ -8,14 +8,28 @@ import com.example.kingpowertest.domain.repository.PhotoRepository
 import com.example.kingpowertest.domain.usecase.GetPhotoUseCase
 import com.example.kingpowertest.presentation.main.mapper.PhotoNetworkModelMapper
 import com.example.kingpowertest.presentation.main.viewmodel.MainFragmentViewModel
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val appModule = module {
 
     single(named(DI_NAME_KingPowerService)) {
-        KingPowerService.instance
+        val client = OkHttpClient().newBuilder()
+//            .addInterceptor(TokenExpiredInterceptor())
+            .build()
+        val instance: KingPowerService by lazy {
+            Retrofit.Builder().baseUrl(BuildConfig.BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+                .build()
+                .create(KingPowerService::class.java)
+        }
+        instance
     }
 
     single(named(DI_NAME_PhotoRemoteDataSourceImpl)) {
